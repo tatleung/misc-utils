d885d331bab7934e0bde2155b803cfcf0853f436
Tat Leung <tatml721@gmail.com>
2021-01-22 22:03:03 -0800

    init

---
+++ include/misc-utils.h
@@ -0,0 +1,8 @@
+#ifndef __MISC_UTILS_H__
+#define __MISC_UTILS_H__
+
+#define MESSAGE "HELLO"
+
+void showMessage();
+
+#endif // __MISC_UTILS_H__
---
+++ src/misc-utils.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include "misc-utils.h"
+
+void showMessage() {
+    printf("The message is %s\n", MESSAGE);
+}
